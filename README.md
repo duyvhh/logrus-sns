244468e8ce25043fb6b7978cfaaee5907f98e60b
stvvan <stevevan@hoiio.com>
2017-12-18 10:42:21 +0700

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# logrus-sns
-AWS SNS hook for Logrus
+# SNS Hook for [Logrus](https://github.com/Sirupsen/logrus)
+
+### Install
+> $ go get github.com/stvvan/logrus_sns
+
+### Usage
+```
+package main
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/stvvan/logrus_sns"
+)
+
+func main() {
+	snsHook, err := logrus_sns.NewSNSHook("topic_arn", "subject", "us-east-1")
+
+	if err != nil {
+		panic(err)
+	}
+
+	log.AddHook(snsHook)
+
+	log.WithFields(log.Fields{
+		"error": "errorMsg",
+	}).Error("error!")
+}
+```
--- sns.go
+++ sns.go
@@ -1,10 +1,12 @@
 package logrus_sns
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sns"
-	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/sirupsen/logrus"
 )
 
@@ -14,26 +16,26 @@ type SNSHook struct {
 	// will not be dispatched. If nil, all messages will be dispatched.
 	AcceptedLevels []logrus.Level
 	Session        *sns.SNS
-	QueueUrl       *string
+	TopicArn       *string
+	Subject        *string
 	Extra          map[string]interface{}
 }
 
 // Levels define the level of logs which will be sent to SNS
-func (sh *SNSHook) Levels() []logrus.Level {
-	if sh.AcceptedLevels == nil {
+func (hook *SNSHook) Levels() []logrus.Level {
+	if hook.AcceptedLevels == nil {
 		return logrus.AllLevels
 	}
 
-	return sh.AcceptedLevels
+	return hook.AcceptedLevels
 }
 
-func NewSNSHook(topic string) (*SNSHook, error) {
-	// Creates a SNS hook with a standard AWS session configured by AWS Environment Variables
-	s := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
+func NewSNSHook(topicArn, subject, region string) (*SNSHook, error) {
+	s, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})
 
-	hook, err := NewSNSHookWithSession(topic, s)
+	hook, err := NewSNSHookWithSession(topicArn, subject, s)
 
 	if err != nil {
 		return nil, err
@@ -42,20 +44,58 @@ func NewSNSHook(topic string) (*SNSHook, error) {
 	return hook, nil
 }
 
-func NewSNSHookWithSession(topic string, sess *session.Session) (*SNSHook, error) {
+func NewSNSHookWithSession(topicArn, subject string, s *session.Session) (*SNSHook, error) {
 	// Creates a SNS hook with a custom AWS session
 	hook := &SNSHook{}
 
-	hook.Session = sqs.New(sess)
+	hook.Session = sns.New(s)
 
-	resultURL, err := hook.Session.GetQueueUrl(&sqs.GetQueueUrlInput{
-		QueueName: aws.String(queueName),
+	_, err := hook.Session.GetTopicAttributes(&sns.GetTopicAttributesInput{
+		TopicArn: aws.String(topicArn),
 	})
+
 	if err != nil {
 		return nil, err
 	}
 
-	hook.QueueUrl = resultURL.QueueUrl
+	hook.TopicArn = aws.String(topicArn)
+	hook.Subject = aws.String(subject)
 
 	return hook, nil
+}
+
+func (hook *SNSHook) Fire(entry *logrus.Entry) error {
+	publishInput := sns.PublishInput{}
+
+	publishInput.TopicArn = hook.TopicArn
+	publishInput.Message = &entry.Message
+	publishInput.Subject = hook.Subject
+
+	data, err := json.Marshal(&entry.Data)
+
+	if err != nil {
+		return fmt.Errorf("failed to serialize log data into JSON: %s", err.Error())
+	}
+
+	publishInput.MessageAttributes = map[string]*sns.MessageAttributeValue{
+		"Level": {
+			DataType:    aws.String("String"),
+			StringValue: aws.String(entry.Level.String()),
+		},
+		"Time": {
+			DataType:    aws.String("String"),
+			StringValue: aws.String(entry.Time.String()),
+		},
+		"Data": {
+			DataType:    aws.String("String"),
+			StringValue: aws.String(string(data)),
+		},
+	}
+	_, err = hook.Session.Publish(&publishInput)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
